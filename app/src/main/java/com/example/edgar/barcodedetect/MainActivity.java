1c9336be72b153d411efc7974d1b9555295070b0
EdgarFabiano <eddy.led@gmail.com>
2017-11-30 01:19:06 -0200

    start

---
+++ app/src/main/java/com/example/edgar/barcodedetect/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.edgar.barcodedetect;
+
+import android.*;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        txtView = (TextView) findViewById(R.id.text) ;
+
+    }
+
+    public void scanBarcode(View view){
+        Intent intent = new Intent(this, ScanBarcodeActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 0){
+            if(resultCode == CommonStatusCodes.SUCCESS){
+                if(data != null){
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    txtView.setText(barcode.displayValue);
+                }
+                else{
+                    txtView.setText("Nenhum código encontrado");
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
